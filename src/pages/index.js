0b97c0b3fd34f7c2ef7b88326b2917696024c6bb
Hari <harikrishna.ms@superops.ai>
2022-06-14 16:36:25 +0530

    Removed header

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,7 +2,6 @@ import * as React from "react";
 import Layout from "../components/layout";
 import { StaticImage } from "gatsby-plugin-image";
 import { useStaticQuery, graphql } from "gatsby";
-import Header from "./header";
 
 const IndexPage = () => {
   const data = useStaticQuery(graphql`
