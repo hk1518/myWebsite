055691ba0e61b0a9a3976886ff4158a30f8c8392
Hari <harikrishna.ms@superops.ai>
2022-06-14 16:56:51 +0530

    Gatsby clean +Yarn

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -21,7 +21,7 @@ export default BlogPage;
 
 export const query = graphql`
   query {
-    allMdx(sort: { fields: frontmatter___date, order: DESC }) {
+    allMdx(sort: { fields: frontmatter___date, order: ASC }) {
       nodes {
         frontmatter {
           date(formatString: "MMMM D, YYYY")
