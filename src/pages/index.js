40b14864bf437d91556ffcd70229b9367540d9e0
Hari <harikrishna.ms@superops.ai>
2022-06-15 21:23:09 +0530

    added .js extentions

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,5 @@
 import * as React from "react";
-import Layout from "../components/layout";
+import Layout from "../components/layout.js";
 import { StaticImage } from "gatsby-plugin-image";
 import { useStaticQuery, graphql } from "gatsby";
 
